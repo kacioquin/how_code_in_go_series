a96a8ae9a6f1fa39492e731675fbe3db97ede00e
Cassio de Freitas <cassio.quintino@gmail.com>
2022-10-03 09:03:22 -0300

    working with string functions, like: HasPrefix, HasSuffix, Contains, Count and len

--- strings/strings.go
+++ strings/strings.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	a := "Hello, Go!"
@@ -11,4 +14,13 @@ func main() {
 
 	s := "Sammy likes declaring strings."
 	fmt.Println(s)
+
+	fmt.Println("This string is in UPPERCASE ->", strings.ToUpper(s))
+	fmt.Println("This other string is in lowercase ->", strings.ToLower(s))
+
+	fmt.Println("This string starts with \"Sammy\"? ->", strings.HasPrefix(s, "Sammy"))
+	fmt.Println("This string ends with \"strings\"? ->", strings.HasSuffix(s, "strings"))
+	fmt.Println("This string contains the word \"likes\"? ->", strings.Contains(s, "likes"))
+	fmt.Println("How many times the letter \"s\" appears in the sentece? ->", strings.Count(s, "s"))
+	fmt.Println("The size of the this sentence is ->", len(s))
 }
