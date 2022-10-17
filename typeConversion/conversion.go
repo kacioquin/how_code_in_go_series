55798d701973ac6ac1273b208874b24c9c3a51b2
Cassio de Freitas <cassio.quintino@gmail.com>
2022-10-17 09:56:10 -0300

    conversions

--- typeConversion/conversion.go
+++ typeConversion/conversion.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	var index int8 = 15
@@ -9,7 +12,6 @@ func main() {
 
 	var bigNumber int64 = 64
 	var littleNumber int8
-
 	littleNumber = int8(bigNumber)
 
 	var x int64 = 57
@@ -19,4 +21,20 @@ func main() {
 	fmt.Printf("bigIndex data type -> %T\n", bigIndex)
 	fmt.Println("little number -> ", littleNumber)
 	fmt.Printf("Int to float conversion %.2f\n", y)
+
+	intToString := strconv.Itoa(10)
+	fmt.Printf("%T\n", intToString)
+	fmt.Printf("%q\n", intToString)
+
+	user := "Sammy"
+	lines := 50
+
+	fmt.Println("Congratulations, " + user + "! You just wrote " + strconv.Itoa(lines) + " lines of code.")
+
+	//floats to string
+	fmt.Println(fmt.Sprint(421.034))
+
+	f := 5524.53
+	fmt.Println(fmt.Sprint(f))
+	fmt.Println("Sammy has " + fmt.Sprint(f) + " points.")
 }
